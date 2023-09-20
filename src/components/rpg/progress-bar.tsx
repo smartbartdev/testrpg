3e4435c993f1288f830df0f6acad0b28e5ee195a
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-20 11:25:09 +0200

    feat: add progress bars

---
+++ src/components/rpg/progress-bar.tsx
@@ -0,0 +1,14 @@
+import {Progress} from "@/components/ui/progress";
+import {Label} from "@/components/ui/label";
+
+export function ProgressBar(props: {value: number, total: number, label: string}) {
+    return (
+        <div className={"space-y-0.5 w-full"}>
+            <Label className={"text-sm text-muted-foreground"}>{props.label}</Label>
+            <div className={"flex items-center space-x-2"}>
+                <Progress value={(props.value / props.total) * 100} className={"bg-blue-50 rounded-sm"}/>
+                <span className={"text-sm text-muted-foreground"}>{props.value}</span>
+            </div>
+        </div>
+    )
+}
