fbdc8ec1362bbaffa6adef8b4286b7d4b880d9cf
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-20 12:55:23 +0200

    feat: add pulse when max level is reached

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-blue-500 transition-all"
+      className={cn("h-full w-full flex-1 transition-all", value === 100 ? "bg-amber-600 animate-pulse" : "bg-blue-500")}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
