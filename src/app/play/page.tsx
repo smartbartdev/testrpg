dc904b4c8fd1d970f41065384c8e6f6a1bdb3ef2
Marcel Blijleven <marcelblijleven@gmail.com>
2023-09-20 11:26:06 +0200

    feat: add play page

---
+++ src/app/play/page.tsx
@@ -0,0 +1,9 @@
+import {CharacterBuilder} from "@/components/rpg/character-builder";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+        <CharacterBuilder />
+    </main>
+  )
+}
